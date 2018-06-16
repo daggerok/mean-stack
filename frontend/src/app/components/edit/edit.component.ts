d056ed987eb22f2d784f92705e18505e1c8af768
Maksim Kostromin <daggerok@gmail.com>
2018-06-16 18:06:05 +0300

    This branch is so dirty, even your mom can't clean it. (C) whatthecommit.com

---
+++ frontend/src/app/components/edit/edit.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit',
+  template: `
+    <p>
+      edit works!
+    </p>
+  `,
+  styles: []
+})
+export class EditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ frontend/src/app/components/list/list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  template: `
+    <p>
+      list works!
+    </p>
+  `,
+  styles: []
+})
+export class ListComponent implements OnInit {
+  constructor() { }
+  ngOnInit() { }
+}
